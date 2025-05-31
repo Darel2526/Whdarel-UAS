4350fb62c94900ae909a6d3fa33692ba1430f87a
Whdarel-UAS <septamadarel@gmail.com>
2025-05-31 16:56:53 +0700

    uas selesai

--- lib/data_saya.dart
+++ lib/data_saya.dart
@@ -1,4 +1,4 @@
 class DataSaya {
-   static String nama = "Kudut";
-    static String gambar = "assets/mahasiswa/kudut.png";
+  static String nama = "Darel";
+  static String gambar = "assets/mahasiswa/darel.jpeg";
 }
